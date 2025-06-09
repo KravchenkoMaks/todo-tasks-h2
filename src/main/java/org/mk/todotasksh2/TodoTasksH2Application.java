60ddc848b530b04fcf715553eafc9169e5dae14a
KravchenkoMaks <maviegma@gmail.com>
2025-06-09 11:08:44 +0300

    init

---
+++ src/main/java/org/mk/todotasksh2/TodoTasksH2Application.java
@@ -0,0 +1,13 @@
+package org.mk.todotasksh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTasksH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTasksH2Application.class, args);
+	}
+
+}
